76c11ac49efd04e88b2e90e082172129d7c5c8eb
Austin <kennedystampsnc28709@gmail.com>
2022-11-21 15:25:14 -0500

    added first three prompts with validation to prevent empty input

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+inquirer.prompt(
+    [
+        {
+            type: 'input',
+            name: 'Project Title',
+            message: 'What is the title of your project?',
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
+        {
+            type: 'input',
+            name: 'Project Description',
+            message: 'What is the description of your project?',
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
+        {
+            type: 'input',
+            name: 'Table of Contents',
+            message: 'Please input the table of contents of your project?',
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
+        {
+
+        }
+
+    
+    ]
+)
