ca76114741966c573b875b0899cf26f3b733fc28
Austin <kennedystampsnc28709@gmail.com>
2022-11-21 16:39:07 -0500

    finished prompt list

--- index.js
+++ index.js
@@ -22,8 +22,43 @@ inquirer.prompt(
             validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
         },
         {
+            type: 'input',
+            name: 'Installation',
+            message: 'How would you like to install your project?',
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'How would you like to use your project?',
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license would you like to use for your project?',
+            choices: ['MIT License', 'GPL License', 'Apache License'],
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
+        {
+            type: 'input',
+            name: 'contributors',
+            message: 'Please list any contributors you would like to include',
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please list any tests you would like to include',
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Please list any questions you would like to include',
+            validate: (value) => { if(value){return true} else {return 'you must input a value to continue'}},
+        },
 
-        }
 
     
     ]
